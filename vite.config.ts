176abc39bf1f65f75f7591ff88d0a72693a0a5b1
Tiago Pinto <tigmmp@hotmail.com>
2022-01-31 22:54:56 +0000

    added test coverage files

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  test: {}
+  test: {
+    coverage: {
+      reporter: ['text', 'json', 'html']
+    }
+  }
 })
