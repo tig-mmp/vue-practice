93df6121da66af2cb075c6c9d287052f45f17cff
Tiago Pinto <tigmmp@hotmail.com>
2022-02-01 22:43:51 +0000

    get customers

--- src/components/ListCustomers.vue
+++ src/components/ListCustomers.vue
@@ -1,3 +1,23 @@
-<template><h2>List of customers</h2></template>
+<template>
+  <h2>List of customers</h2>
+  <div v-for="customer in customers" :key="customer.id">
+    {{ customer.name }}
+  </div>
+</template>
 
-<script></script>
+<script setup lang="ts">
+import { useAxios } from "@vueuse/integrations/useAxios";
+
+const {
+  data: customers,
+}: {
+  data: {
+    id: number;
+    name: string;
+    country: {
+      name: string;
+      code: string;
+    };
+  }[];
+} = useAxios("http://localhost:3001/customers");
+</script>
