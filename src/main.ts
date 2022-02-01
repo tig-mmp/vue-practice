8307a64e10c87efc47bbcd40a602f66988d814c8
Tiago Pinto <tigmmp@hotmail.com>
2022-02-01 22:39:46 +0000

    fixed "app.use is not a function"

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import {createApp} from 'vue';
-import App from './App.vue';
-import 'primevue/resources/themes/saga-blue/theme.css';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.min.css';
+import { createApp } from "vue";
+import App from "./App.vue";
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.min.css";
 
-
-import PrimeVue from 'primevue/config';
-const app = createApp(App).mount('#app');
+import PrimeVue from "primevue/config";
+const app = createApp(App);
 
 app.use(PrimeVue);
+app.mount("#app");
