7a142ba749f26880c3686e89785b9bdddb420ec4
Tiago Pinto <tigmmp@hotmail.com>
2022-02-03 20:59:15 +0000

    added vue-country-flag-next

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.min.css";
 
-import PrimeVue from "primevue/config";
 const app = createApp(App);
 
+import PrimeVue from "primevue/config";
 app.use(PrimeVue);
+
 app.mount("#app");
