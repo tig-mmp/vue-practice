94f1af350e4e12a35d073cc63a971c9559d616a1
Tiago Pinto <tiago.pinto@parcialfinance.com>
2022-07-13 00:29:56 +0100

    useAxios uses interface as type

--- src/components/ListCustomers.vue
+++ src/components/ListCustomers.vue
@@ -1,28 +1,16 @@
 <template>
-  <DataTable
-    :value="customers"
-    paginator
-    :rows="10"
-    dataKey="id"
-    rowHover
+  <DataTable :value="customers" paginator :rows="10" dataKey="id" rowHover
     paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
     :rowsPerPageOptions="[10, 25, 50, 100, 500, 100]"
-    currentPageReportTemplate="Showing {first} to {last} of {totalRecords} entries"
-    responsiveLayout="scroll"
-    v-model:filters="filters"
-    filterDisplay="menu"
-    :globalFilterFields="['name', 'country.name']"
-    :loading="!isFinished"
-  >
+    currentPageReportTemplate="Showing {first} to {last} of {totalRecords} entries" responsiveLayout="scroll"
+    v-model:filters="filters" filterDisplay="menu" :globalFilterFields="['name', 'country.name']"
+    :loading="!isFinished">
     <template #header>
       <div class="flex justify-content-between">
         <h2 class="m-0">Customers</h2>
         <span class="p-input-icon-left">
           <i class="pi pi-search" />
-          <InputText
-            v-model="filters['global'].value"
-            placeholder="Keyword Search"
-          />
+          <InputText v-model="filters['global'].value" placeholder="Keyword Search" />
         </span>
       </div>
     </template>
@@ -33,12 +21,7 @@
         {{ data.name }}
       </template>
     </Column>
-    <Column
-      field="country.name"
-      header="Country"
-      sortable
-      filterField="country.name"
-    >
+    <Column field="country.name" header="Country" sortable filterField="country.name">
       <template #body="{ data }">
         <country-flag :country="data.country.code" />
         <span class="ml-3">{{ data.country.name }}</span>
@@ -48,29 +31,22 @@
 </template>
 
 <script setup lang="ts">
-import CountryFlag from "vue-country-flag-next";
 import { useAxios } from "@vueuse/integrations/useAxios";
-import DataTable from "primevue/datatable";
-import Column from "primevue/column";
-import { ref } from "vue";
-import InputText from "primevue/inputtext";
 import { FilterMatchMode } from "primevue/api";
+import Column from "primevue/column";
+import DataTable from "primevue/datatable";
+import InputText from "primevue/inputtext";
+import { ref } from "vue";
+import CountryFlag from "vue-country-flag-next";
+import { Country } from "../interfaces/Country";
 
 const filters = ref({
   global: { value: null, matchMode: FilterMatchMode.CONTAINS },
 });
+
 const {
   data: customers,
   isFinished,
-}: {
-  data: {
-    id: number;
-    name: string;
-    country: {
-      name: string;
-      code: string;
-    };
-  }[];
-  isFinished: boolean;
-} = useAxios("http://localhost:3001/customers");
+} = useAxios<Country[]>("http://localhost:3001/customers");
+
 </script>
